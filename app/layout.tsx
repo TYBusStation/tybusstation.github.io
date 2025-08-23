19ccadfd637464bf74270028c78b92940261d851
myster7494 <jackychiu0101@gmail.com>
2025-08-23 13:47:03 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
 import "./globals.css";
 
-import Navbar from "./(components)/Navbar";
 import Footer from "./(components)/Footer";
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
+    return (
+        <html lang="en">
+        {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>
+        <head/>
+        <body>
         {/* <Navbar /> */}
-        <div className="min-h-screen bg-gradient-to-t from-zinc-800 to-zinc-900 p-2 md:flex items-center justify-center">
-          {children}
+        <div
+            className="min-h-screen bg-gradient-to-t from-zinc-800 to-zinc-900 p-2 md:flex items-center justify-center">
+            {children}
         </div>
-        <Footer />
-      </body>
-    </html>
-  );
+        <Footer/>
+        </body>
+        </html>
+    );
 }
