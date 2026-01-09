0ac0f5d6394ee23fc39a98af84b79e2bf7e86d42
myster7494 <jackychiu0101@gmail.com>
2026-01-09 23:06:45 +0800

    update

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -12,12 +12,12 @@ export default function Discord() {
               content="公車交流群"/>
         <meta property="og:image:alt"
               content="公車交流群"/>
-        <meta name="og:image" content="../avatar.png"/>
-        <meta name="twitter:image" content="../avatar.png"/>
+        <meta name="og:image" content="./avatar.png"/>
+        <meta name="twitter:image" content="./avatar.png"/>
         <meta property="og:image:width" content="1600"/>
         <meta property="og:image:height" content="1600"/>
         <meta property="og:url" content="https://tybusstation.github.io/"/>
         <meta name="twitter:card" content="summary_large_image"/>
-        <meta http-equiv="refresh" content="0;url=https://discord.gg/J9H6aUG7Cs"/>
+        <meta http-equiv="refresh" content="0;url=https://discord.gg/nH7E28CVaF"/>
     </Head>
 }
